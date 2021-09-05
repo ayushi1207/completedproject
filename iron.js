65365a3e24ded8856a118bbb147582a4871b6d10
ayushi1207 <71817481+ayushi1207@users.noreply.github.com>
2021-09-05 11:57:58 +0530

    Update iron.js

--- iron.js
+++ iron.js
@@ -14,8 +14,13 @@ class iron {
     
     display(){
       var pos =this.body.position;
+var angle = this.body.angle;
+     translate(pos.x, pos.y);
+      rotate(angle);
       rectMode(CENTER);
-      fill("grey");
-      rect(pos.x, pos.y, this.width, this.height);
+
+      fill("brown");
+      rect(0, 0, this.width, this.height);
+      pop();
     }
   };
